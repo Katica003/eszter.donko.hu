af64712abc94564ef6b926ff5e2d03998aea2a1d
István Donkó <istvan.donko@gmail.com>
2025-04-30 11:05:04 +0200

    Set up basic infrastructure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  devToolbar: { enabled: false },
+  site: 'https://eszter.donko.hu',
+  // TODO: Maybe use `vite-tsconfig-paths` instead?
+  vite: { resolve: { alias: { '@': '/src' } } },
+})
